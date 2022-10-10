f464269aa3f7ae3e0b372e0ba1b249c1b9144537
ESPEDUZA <eliott.germain@hotmail.fr>
2022-10-10 19:29:06 +0200

    first commit of sdl2 lib test

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./sources/file_mapping/file_mapping.h"
-
+#include "sdlTest/sedlTest.h"
 int main(int argc, char const *argv[])
 {
-    fileMapping("./ressources/maps/config.rtbob");
+    //fileMapping("./ressources/maps/config.rtbob");
+    sdlTest();
+    printf("hello world");
     return 0;
 }
