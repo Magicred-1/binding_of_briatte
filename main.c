ee16152e5cc7d44326457b4f164b7ff884b18659
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-10-10 15:23:12 +0200

    Revert "[ADD] First commit"

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "./sources/file_mapping/file_mapping.h"
-
-int main(int argc, char const *argv[])
-{
-    fileMapping("./ressources/maps/config.rtbob");
-    return 0;
-}
