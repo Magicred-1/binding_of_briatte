12846a958760db91ad2f61e9a6dd9232794a4eed
Wizer <84985036+WizerZ@users.noreply.github.com>
2022-11-14 20:25:54 +0100

    Add files via upload

---
+++ ressources/read_monsters/read_monster.c
@@ -0,0 +1,97 @@
+#include "read_monster.h"
+#include "../more_string/more-string.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+Monster* newMonster(char* name,  float hpmax, char* shoot, char* ss, char* flight){
+    Monster* m = malloc(sizeof(Monster));
+    m->name = duplicateString(name);
+    m->hpMax = hpmax;
+    m->shoot = duplicateString(shoot);
+    m->ss = duplicateString(ss);
+    m->flight = duplicateString(flight);
+    return m;
+}
+
+void printMonster(Monster monster){
+    printf("name: %s \nhpmax: %.1f \nshoot: %s \nspectral shoot: %s \nflight: %s", monster.name, monster.hpMax, monster.shoot, monster.ss, monster.flight);
+}
+
+void printMonsterArray(Monster** arrayMonster, int nbOfMonsters){
+    for(int i = 0; i < nbOfMonsters; i += 1){
+        printf("name: %s \nhpmax: %.1f \nshoot: %s \nspectral shoot: %s \nflight: %s\n", arrayMonster[i]->name, arrayMonster[i]->hpMax, arrayMonster[i]->shoot, arrayMonster[i]->ss, arrayMonster[i]->flight);
+    }
+}
+
+
+void freeMonsters(Monster** monster){
+    for(int i = 0; i < 5; i += 1){
+        free(monster[i]->name);
+        free(monster[i]->shoot);
+        free(monster[i]->ss);
+        free(monster[i]->flight);
+        free(monster[i]);
+    }
+    free(monster);
+}
+
+Monster** monstersMap(char *sourceFile, int* ptrNbOfMonster){
+    FILE* f = fopen(sourceFile, "r");
+    if(f != NULL){
+        fscanf(f, "{%d}", ptrNbOfMonster);
+        Monster **array = malloc(sizeof(Monster)**ptrNbOfMonster);
+        fseek(f, 2, SEEK_CUR);
+
+        for(int i = 0; i < 5; i += 1){
+            char name[20];
+            float hpMax = 0;
+            char shoot[10] = "false";
+            char ss[10] = "false";
+            char flight[10] = "false";
+
+            fseek(f, 5, SEEK_CUR);
+            fscanf(f, "name=%s", name);
+
+            fseek(f, 2, SEEK_CUR);
+            fscanf(f, "hpMax=%f", &hpMax);
+
+            fseek(f, 2, SEEK_CUR);
+            if(getc(f) != '-'){
+                fseek(f, -1, SEEK_CUR);
+                if(getc(f) == 's'){
+                    fseek(f, -1, SEEK_CUR);
+                    fscanf(f,"shoot=%s", shoot);
+                }
+                fseek(f, 2, SEEK_CUR);
+                if(getc(f) != '-'){
+                    fseek(f, -1, SEEK_CUR);
+                    if(getc(f) == 's'){
+                        fseek(f, -1, SEEK_CUR);
+                        fscanf(f,"ss=%s", ss);
+                    }
+                    fseek(f, -1, SEEK_CUR);
+                    if(getc(f) == 'f'){
+                        fseek(f, -1, SEEK_CUR);
+                        fscanf(f,"flight=%s", flight);
+                        fseek(f, 2, SEEK_CUR);
+                    }else{
+                        fseek(f, -1, SEEK_CUR);
+                    }
+                }
+                else{
+                    fseek(f, -1, SEEK_CUR);
+                }
+            }else{
+                fseek(f, -1, SEEK_CUR);
+            }
+            array[i] = newMonster(name, hpMax, shoot, ss, flight);
+        }
+        fclose(f);
+        return array;
+    }else {
+        printf("Erreur lors de l'ouverture du fichier");
+        return NULL;
+    }
+
+}
---
+++ ressources/read_monsters/read_monster.h
@@ -0,0 +1,44 @@
+/*
+**  Filename : read_monster.h
+**
+**  Made by : Alexis Duquenne <alexisduquenne01@gmail.com>
+**
+**  Description : Create a map (a two dimension array) from a file
+**  to be use in the game to define monsters.
+*/
+
+#ifndef READ_MONSTER_H
+#define READ_MONSTER_H
+
+struct Monster
+{
+    char* name;      //name of the monster
+    float hpMax;          //heal point max of the monster
+    char* shoot;          //If the monster is able to shoot
+    char* ss;             //If the monster have spectral shoot
+    char* flight;         //If the monster is able to fly
+};
+
+typedef struct Monster Monster;
+
+/**
+ *
+*/
+Monster* newMonsters(char* name, float hpmax, char* shoot, char* ss, char* flight);
+
+/**
+ * @param sourceFile Path of the source file which contains monsters
+*/
+Monster** monstersMap(char *sourceFile, int *ptrNbOfMonsters);
+
+
+/**
+** @param monster : the array to be printed
+*/
+void printMonster(Monster monster);
+
+void printMonsterArray(Monster** monster, int NbOfMonster);
+
+void freeMonsters(Monster** monster);
+
+#endif //READ_MONSTER_H
